1daeffe97f41d5a17b4f63de6a8f81f833657cd6
Stanislav V <5777227@gmail.com>
2024-10-14 15:47:32 +0300

    add all api elements on the index

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,26 @@ import type { AxiosInstance } from 'axios';
 
 import createAdminApi from './admin';
 import createAuthorizationApi from './authorization';
+import createDefaultApi from './default';
+import createLeaderboardApi from './leaderboard';
+import createOfferwallApi from './offerwall';
+import createOfflineEarningApi from './offlineEarning';
+import createReferralsApi from './referrals';
 import createUserApi from './user';
+import createWalletApi from './wallet';
 
 function createApi(instanceAxios: AxiosInstance) {
     return {
-        userApi: createUserApi(instanceAxios),
+
         adminApi: createAdminApi(instanceAxios),
-        authorizationApi: createAuthorizationApi(instanceAxios)
+        authorizationApi: createAuthorizationApi(instanceAxios),
+        defaultApi: createDefaultApi(instanceAxios),
+        leaderboardApi: createLeaderboardApi(instanceAxios),
+        offerwallApi: createOfferwallApi(instanceAxios),
+        offlineEarningApi: createOfflineEarningApi(instanceAxios),
+        referralsApi: createReferralsApi(instanceAxios),
+        userApi: createUserApi(instanceAxios),
+        walletApi: createWalletApi(instanceAxios)
     };
 }
 export default createApi;
